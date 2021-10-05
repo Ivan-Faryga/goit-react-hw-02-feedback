38dca9b021ce530772f7a048aa5ef26cfd9df87a
Ivan Faryga <ifaryga@gmail.com>
2021-10-06 00:32:27 +0300

    task done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Feedbackoptions from "./components/FeedbackOptions/FeedbackOptions";
+import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
 import Section from "./components/Section/Section";
 import Statistics from "./components/Statistics/Statistics";
-import Notification from "./components/Notification/Notification";
+import "./App.css";
 
 class App extends Component {
   state = {
@@ -34,45 +34,62 @@ class App extends Component {
 
   render() {
     const { good, neutral, bad } = this.state;
-    const { total } = this.countTotalFeedbackAmount();
-    const { positiveFeedback } = this.countPositiveFeedbackPercentage();
+    // const { total } = this.countTotalFeedbackAmount();
+    // const { positiveFeedback } = this.countPositiveFeedbackPercentage();
+
     return (
-      <div>
-        <h2>Statistics</h2>
-        <div>
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              percentage={positiveFeedback}
-            />
-          ) : (
-            <Notification message="No feedback given" />
-          )}
-        </div>
-        {/* <h2>PLease leave feedback</h2>
-        <button type="button" name="good" onClick={this.handleClick}>
-          Good
-        </button>
-        <button type="button" name="neutral" onClick={this.handleClick}>
-          Neutral
-        </button>
-        <button type="button" name="bad" onClick={this.handleClick}>
-          Bad
-        </button>
-        <h2>Statistics</h2>
-        <ul>
-          <li>Good: {good}</li>
-          <li>Neutral: {neutral}</li>
-          <li>Bad: {bad}</li>
-          <li>Total: {this.countTotalFeedbackAmount()}</li>
-          <li>Positive feedback: {this.countPositiveFeedbackPercentage()} %</li>
-        </ul> */}
+      <div className="wrapper">
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.handleClick}
+        />
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedbackAmount()} // почему не отрабатывал вариант с деструктуризацией????
+            percentage={this.countPositiveFeedbackPercentage()} // почему не отрабатывал вариант с деструктуризацией????
+          />
+        </Section>
       </div>
     );
   }
 }
+
+// {
+//   total > 0 ? (
+//     <Statistics
+//       good={good}
+//       neutral={neutral}
+//       bad={bad}
+//       total={total}
+//       percentage={positiveFeedback}
+//     />
+//   ) : (
+//     <Notification message="No feedback given" />
+//   );
+// }
+
+// {
+//   /* <h2>PLease leave feedback</h2>
+//         <button type="button" name="good" onClick={this.handleClick}>
+//           Good
+//         </button>
+//         <button type="button" name="neutral" onClick={this.handleClick}>
+//           Neutral
+//         </button>
+//         <button type="button" name="bad" onClick={this.handleClick}>
+//           Bad
+//         </button>
+//         <h2>Statistics</h2>
+//         <ul>
+//           <li>Good: {good}</li>
+//           <li>Neutral: {neutral}</li>
+//           <li>Bad: {bad}</li>
+//           <li>Total: {this.countTotalFeedbackAmount()}</li>
+//           <li>Positive feedback: {this.countPositiveFeedbackPercentage()} %</li>
+//         </ul> */
+// }
 
 export default App;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,33 @@
 import PropTypes from "prop-types";
+import Notification from "../Notification/Notification";
+import s from "./Statistics.module.css";
 
-const Statistics = ({ good, neutral, bad, total, percentage }) => {
-  return (
-    <ul>
-      <li>Good: {good}</li>
-      <li>Neutral: {neutral}</li>
-      <li>Bad: {bad}</li>
-      <li>Total: {total}</li>
-      <li>Positive feedback: {percentage} %</li>
-    </ul>
-  );
-};
+const Statistics = ({ good, neutral, bad, total, percentage }) => (
+  <div>
+    {total > 0 ? (
+      <ul className={s.list}>
+        <li className={s.item}>
+          <span className={s.positive}>Good: {good}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.middle}>Neutral: {neutral}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.negative}>Bad: {bad}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.sum}>Total: {total}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.percents}>Positive feedback: {percentage} %</span>
+        </li>
+      </ul>
+    ) : (
+      <Notification message={"No feedback given"} />
+    )}
+  </div>
+);
+
 export default Statistics;
 
 Statistics.propTypes = {
